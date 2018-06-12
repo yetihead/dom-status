96829106d3cdfed02443ec2e68d82db122949f6a
yetihead <yetibrain@yandex.ru>
2018-06-12 21:46:24 +0300

    add custom greeting

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const http = require('http');
 const page = require('./lib/page');
 const dom = require('./lib/dom');
 const PORT = process.env.PORT || 3000;
+const greeting = process.env.greeting || 'Hello, user!';
 
 const server = http.createServer((req, res) => {
 
   dom.getStatus().then(status => {
-    res.end(page.render(status));
+    res.end(page.render({
+      content: status,
+      greeting
+    }));
   })
 });
 
